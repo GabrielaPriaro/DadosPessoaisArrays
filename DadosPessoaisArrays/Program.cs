9cda49c9d66ab3bd8c2f3df527519eddc1231af6
Gabriela <gabriela.priaro@gmail.com>
2024-11-05 11:18:23 +0000

    Adicionar arquivos de projeto.

---
+++ DadosPessoaisArrays/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace DadosPessoaisArrays
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
